d0e7feb64bdd00f5d3b446f2468cf8019dadbb08
Amir Alavi <aalavi@andrew.cmu.edu>
2017-05-16 17:41:30 -0400

    Reads in input and extracts labels and feature mat
    Need to add ability to normalize by gene and by sample.

--- preprocessing.py
+++ preprocessing.py
@@ -3,16 +3,31 @@ import numpy as np
 
 
 class DataContainer(object):
-    """A POD object that contains the parsed data from a gene expression file.
+    """Parses and holds the input data table (gene expression file) in memory
+    and provides access to various aspects of it.
     """
-    __init__(self, filename):
+    def __init__(self, filepath, sample_normalize=False, gene_normalize=False):
         # Use pandas.read_csv to read in the file
+        print('Reading in data from ', filepath)
+        dataframe = pd.read_csv(filepath, sep='\t', index_col=0, header=0, comment='#')
+        dataframe = dataframe.T
+        self.dataframe = dataframe
+
+
+    def get_labeled_data(self):
+        print("getting labeled data")
+        labeled_data = self.dataframe.loc[lambda df: df.Label != 'None', :]
+        # Column 0 is Label and Column 1 is Weight. Columns after these are
+        # the genes. Select these columns (and convert them to floats).
+        converter = lambda series: pd.to_numeric(series, downcast='float')
+        expression_mat = labeled_data.iloc[:, 2:].apply(converter).values
+
+        label_strings = labeled_data.loc[:, 'Label'].values
+        # Need to encode the labels as integers (like categorical data)
+        # Can do this by getting a list of unique labels, then for each sample,
+        # convert it's label to that label's index in this list.
+        uniq_label_strings, labels_as_int = np.unique(label_strings, return_inverse=True)
+        return expression_mat, labels_as_int, uniq_label_strings
+
+    def get_unlabeled_data(self):
         pass
-
-
-def load_data(file_path):
-    """
-    Returns:
-
-    """
-    lines = open(filename).readlines()
--- scrna.py
+++ scrna.py
@@ -2,7 +2,7 @@
 
 Usage:
     scrna.py train <neural_net_architecture> <hidden_layer_sizes>... [--act=<activation_fcn> --epochs=<nepochs> --data=<path>]
-    scrna.py evaluate 
+    scrna.py evaluate
     scrna.py (-h | --help)
     scrna.py --version
 
@@ -12,22 +12,28 @@ Options:
     --epochs=<nepochs>      Number of epochs to train for [default: 20]. Only used
                             for 'train' command.
     --act=<activation_fcn>  Activation function to use for the layers [default: tanh].
-    --data=<path>           Path to input data file [default: data/TPM_mouse_7_8_10_PPITF_gene_9437.txt]. 
+    --data=<path>           Path to input data file [default: data/TPM_mouse_7_8_10_PPITF_gene_9437.txt].
 
 """
 from docopt import docopt
 
 from util import ScrnaException
 from neural_nets import get_nn_model
+from preprocessing import DataContainer
+
+def train(args):
+    data = DataContainer(args['--data'])
+    data.get_labeled_data()
+    hidden_layer_sizes = [int(x) for x in args['<hidden_layer_sizes>']]
+    model = get_nn_model(args['<neural_net_architecture>'], hidden_layer_sizes, 100, args['--act'])
+    print(model.summary())
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='scrna 0.1')
     print(args); print()
     try:
         if args['train']:
-            hidden_layer_sizes = [int(x) for x in args['<hidden_layer_sizes>']]
-            model = get_nn_model(args['<neural_net_architecture>'], hidden_layer_sizes, 100, args['--act'])
-            print(model.summary())
+            train(args)
     except ScrnaException as e:
         msg = e.args[0]
         print("scrna excption: ", msg)
