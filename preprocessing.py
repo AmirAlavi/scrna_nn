0b8fa9316d1dc37ec3ba8c9cd36cf010ca866ca5
Amir Alavi <aalavi@andrew.cmu.edu>
2017-05-17 14:17:06 -0400

    Can now normalize each sample, standardize each gene.

--- preprocessing.py
+++ preprocessing.py
@@ -1,31 +1,45 @@
+#import pdb; pdb.set_trace()
+import time
+
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler, Normalizer
 
 
 class DataContainer(object):
     """Parses and holds the input data table (gene expression file) in memory
     and provides access to various aspects of it.
     """
-    def __init__(self, filepath, sample_normalize=False, gene_normalize=False):
+    def __init__(self, filepath, sample_normalize=False, gene_standardize=False):
         # Use pandas.read_csv to read in the file
         print('Reading in data from ', filepath)
         dataframe = pd.read_csv(filepath, sep='\t', index_col=0, header=0, comment='#')
         dataframe = dataframe.T
+        # Column 0 is Label and Column 1 is Weight. Columns after these are
+        # the genes. Select numeric columns (and convert them to floats)
+        # (downcast because float64 might not be needed).
+        converter = lambda series: pd.to_numeric(series, downcast='float')
+        dataframe.iloc[:,1:] = dataframe.iloc[:,1:].apply(converter)
+        if sample_normalize:
+            normalizer = Normalizer(norm='l1')
+            dataframe.iloc[:, 2:] = normalizer.fit_transform(dataframe.iloc[:, 2:])
+        if gene_standardize:
+            # Standardize each column by centering and having unit std
+            scaler = StandardScaler()
+            dataframe.iloc[:, 2:] = scaler.fit_transform(dataframe.iloc[:, 2:])
         self.dataframe = dataframe
 
 
     def get_labeled_data(self):
         print("getting labeled data")
         labeled_data = self.dataframe.loc[lambda df: df.Label != 'None', :]
-        # Column 0 is Label and Column 1 is Weight. Columns after these are
-        # the genes. Select these columns (and convert them to floats).
-        converter = lambda series: pd.to_numeric(series, downcast='float')
-        expression_mat = labeled_data.iloc[:, 2:].apply(converter).values
-
+        expression_mat = labeled_data.iloc[:, 2:].values
+        print(expression_mat.shape)
+        print(expression_mat)
         label_strings = labeled_data.loc[:, 'Label'].values
         # Need to encode the labels as integers (like categorical data)
         # Can do this by getting a list of unique labels, then for each sample,
-        # convert it's label to that label's index in this list.
+        # convert it's label to that label's index in this list. Oneliner:
         uniq_label_strings, labels_as_int = np.unique(label_strings, return_inverse=True)
         return expression_mat, labels_as_int, uniq_label_strings
 
--- scrna.py
+++ scrna.py
@@ -1,7 +1,7 @@
 """Single-cell RNA-seq Analysis Pipeline.
 
 Usage:
-    scrna.py train <neural_net_architecture> <hidden_layer_sizes>... [--act=<activation_fcn> --epochs=<nepochs> --data=<path>]
+    scrna.py train <neural_net_architecture> <hidden_layer_sizes>... [--act=<activation_fcn> --epochs=<nepochs> --data=<path> --sn --gs]
     scrna.py evaluate
     scrna.py (-h | --help)
     scrna.py --version
@@ -13,6 +13,8 @@ Options:
                             for 'train' command.
     --act=<activation_fcn>  Activation function to use for the layers [default: tanh].
     --data=<path>           Path to input data file [default: data/TPM_mouse_7_8_10_PPITF_gene_9437.txt].
+    --sn                    Divide each sample by the total number of reads for that sample.
+    --gs                    Subtract the mean and divide by standard deviation within each gene.
 
 """
 from docopt import docopt
@@ -22,7 +24,7 @@ from neural_nets import get_nn_model
 from preprocessing import DataContainer
 
 def train(args):
-    data = DataContainer(args['--data'])
+    data = DataContainer(args['--data'], args['--sn'], args['--gs'])
     data.get_labeled_data()
     hidden_layer_sizes = [int(x) for x in args['<hidden_layer_sizes>']]
     model = get_nn_model(args['<neural_net_architecture>'], hidden_layer_sizes, 100, args['--act'])
