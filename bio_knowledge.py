6ac11abb39852bd9257fa316ee3c93b15c4fecfb
Amir Alavi <aalavi@andrew.cmu.edu>
2017-07-10 13:53:43 -0400

    Preparing PPI/TF grouping code for new data.
    Should now be able to handle the new PPI/TF groups for the new data
    (mouse genes) as well as maintain compatibility with old data.

--- bio_knowledge.py
+++ bio_knowledge.py
@@ -25,7 +25,7 @@ def get_groupings_for_genes(ppi_tf_groups_filepath, dataset_gene_names):
     dataset_gene_names = dataset_gene_names.tolist()
     print("num gene names: ", len(dataset_gene_names))
     lines = open(ppi_tf_groups_filepath).readlines()
-    ppi_tf_groups_as_indicies = defaultdict(list)
+    ppi_tf_groups_as_indices = defaultdict(list)
     for line in lines:
         # Get tab separated tokens in the line
         tokens = line.replace('\n', '').replace('\r', '').split('\t')
@@ -34,11 +34,48 @@ def get_groupings_for_genes(ppi_tf_groups_filepath, dataset_gene_names):
         group_name = tokens[0]
         # The rest of the tokens are the names of the genes in that group
         for gene in tokens[1:]:
-            ppi_tf_groups_as_indicies[group_name].append(dataset_gene_names.index(gene))
-    largest_index = max(map(max, ppi_tf_groups_as_indicies.values()))
-    sorted_group_names = sorted(ppi_tf_groups_as_indicies.keys())
+            if gene in dataset_gene_names:# Decoupling the set of genes in dataset from set of genes in PPITF knowledge
+                ppi_tf_groups_as_indices[group_name].append(dataset_gene_names.index(gene))
+    largest_index = max(map(max, ppi_tf_groups_as_indices.values()))
+    sorted_group_names = sorted(ppi_tf_groups_as_indices.keys())
     binary_group_membership_mat = np.zeros((len(sorted_group_names), largest_index+1), dtype='float32')
     for group_idx, group_name in enumerate(sorted_group_names):
-        for gene_idx in ppi_tf_groups_as_indicies[group_name]:
+        for gene_idx in ppi_tf_groups_as_indices[group_name]:
             binary_group_membership_mat[group_idx, gene_idx] = 1
-    return ppi_tf_groups_as_indicies, sorted_group_names, binary_group_membership_mat
+    return ppi_tf_groups_as_indices, sorted_group_names, binary_group_membership_mat
+
+'''
+def get_groupings_for_genes_new(dataset_gene_names, tf_master_list, ppi_master_list):
+    ppi_tf_groups_as_indices = defaultdict(list)
+    # build TF groups
+    #tf_groups = defaultdict(list)
+    with open(tf_master_list, 'r') as f:
+        lines = f.readlines()
+    for line in lines[1:]: # file has a header
+        stripped = line.rstrip().lower().split('\t')
+        tf = 'tf_' + stripped[0]
+        gene = stripped[1].split(';')[0]
+        if gene in dataset_gene_names:
+            #tf_groups[tf].append(gene)
+            ppi_tf_groups_as_indices[tf].append(dataset_gene_names.index(gene))
+    # build PPI groups
+    #ppi_groups = defaultdict(list)
+    with open(ppi_master_list, 'r') as f:
+        lines = f.readlines()
+    count = 0
+    for line in lines:
+        stripped = line.rstrip().lower().split('\t')
+        ppi = 'ppi_' + str(count)
+        for gene in stripped:
+            if gene in dataset_gene_names:
+                #ppi_groups[tf].append(gene)
+                ppi_tf_groups_as_indices[tf].append(dataset_gene_names.index(gene))
+        count += 1
+    largest_index = max(map(max, ppi_tf_groups_as_indices.values()))
+    sorted_group_names = sorted(ppi_tf_groups_as_indices.keys())
+    binary_group_membership_mat = np.zeros((len(sorted_group_names), largest_index+1), dtype='float32')
+    for group_idx, group_name in enumerate(sorted_group_names):
+        for gene_idx in ppi_tf_groups_as_indices[group_name]:
+            binary_group_membership_mat[group_idx, gene_idx] = 1
+    return ppi_tf_groups_as_indices, sorted_group_names, binary_group_membership_mat
+''' 
