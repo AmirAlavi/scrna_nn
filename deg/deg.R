dc1c7ecdfd7f92c126030d8cd9cea5459ec55b40
Amir Alavi <aalavi@andrew.cmu.edu>
2017-08-11 11:48:11 -0400

    First working version of DEG analysis using limma-voom

--- deg/deg.R
+++ deg/deg.R
@@ -15,6 +15,13 @@ print(str(counts_mat))
 print(dim(counts_mat))
 
 print("Filtering...")
+# Remove cells with fewer than 1.8e3 lib size
+csums <- colSums(counts_mat)
+filter <- csums >= 1.8e3
+cat("Number of filtered out cells: ", sum(!filter), "\n")
+counts_mat <- counts_mat[,filter]
+labels_r <- labels_r[filter]
+accessions_r <- accessions_r[filter]
 # Remove genes which have less than 10 reads accross all samples
 rsums <- rowSums(counts_mat)
 filter <- rsums >= 10
@@ -36,6 +43,7 @@ print(table(accessions_factor_vector))
 
 for(type in levels(type_factor_vector)){
 	 cat("Current type: ", type, "\n")
+	 name_for_results_file <- strsplit(type, split=" ")[[1]][1]
 	 cur_type_selection_vector <- type_factor_vector == type
 	 other_types_selection_vector <- !cur_type_selection_vector
 	 cat("\t", "Number of ", type, " cells: ", sum(cur_type_selection_vector), "\n")
@@ -44,7 +52,10 @@ for(type in levels(type_factor_vector)){
 	 cur_type_experiments = accessions_factor_vector[cur_type_selection_vector]
 	 uniq_cur_type_experiments = unique(cur_type_experiments)
 	 cat("\t", "Number of different experiments for ", type, " ", length(uniq_cur_type_experiments), "\n")
+	 exp_count <- 1
 	 for(exp in uniq_cur_type_experiments){
+	 	 name_for_results_file <- paste(name_for_results_file, exp_count, sep="_")
+		 name_for_results_file <- paste(name_for_results_file, ".txt", sep="")
 	 	 cur_exp_selection_vector <- accessions_factor_vector == exp
 
 		 # Create two counts matrices
@@ -69,8 +80,10 @@ for(type in levels(type_factor_vector)){
 		 print("eBayes...")
 		 fit <- eBayes(fit)
 		 print("topTable...")
-		 print(topTable(fit, coef="CURvsOTHER"))
-		 
+		 results <- topTable(fit, coef="CURvsOTHER", p.value=0.05, number=Inf)
+		 print(typeof(results))
+		 print(results)
+		 write.table(results, name_for_results_file, sep="\t")
 	 }
 }
 
