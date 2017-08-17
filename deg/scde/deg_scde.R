d3b981ba2757775f65d240609109ae3d22a482a6
Amir Alavi <aalavi@andrew.cmu.edu>
2017-08-17 19:41:44 -0400

    Working version of SCDE DEG analysis

--- deg/scde/deg_scde.R
+++ deg/scde/deg_scde.R
@@ -1,6 +1,7 @@
 library(scde)
 
-n.cores <- 16
+n.cores <- 32
+cat("Using ", n.cores, " cores", "\n")
 
 # Load R objects containing data into workspace
 load("counts_mat.gzip")		# counts_mat_r
@@ -11,23 +12,30 @@ print("Done loading data objects")
 
 counts_mat <- data.matrix(counts_mat_r, rownames.force=TRUE)
 counts_mat <- t(counts_mat)
-rownames(counts_mat) <- unname(gene_symbols_r)
+#rownames(counts_mat) <- unname(gene_symbols_r)
 print(str(counts_mat))
 print(dim(counts_mat))
 
 print("Filtering...")
-# Remove cells with fewer than 1.8e5 lib size
-csums <- colSums(counts_mat)
-filter <- csums >= 1.8e5
-cat("Number of filtered out cells: ", sum(!filter), "\n")
+# Remove cells with fewer than 1.8e3 lib size
+csums <- colSums(counts_mat > 0)
+filter <- csums >= 1.8e3
+cat("Number of filtered out cells, detected less than 1.8e3 genes: ", sum(!filter), "\n")
 counts_mat <- counts_mat[,filter]
 labels_r <- labels_r[filter]
 accessions_r <- accessions_r[filter]
 # Remove genes which have less than 10 reads accross all samples
 rsums <- rowSums(counts_mat)
 filter <- rsums >= 10
-cat("Number of filtered out genes: ", sum(!filter), "\n")
+cat("Number of filtered out genes, have less than 10 reads across all samples: ", sum(!filter), "\n")
 counts_mat <- counts_mat[filter,]
+gene_symbols_r <- gene_symbols_r[filter]
+# Remove genes that aren't detected in at least 5 cells
+rsums <- rowSums(counts_mat > 0)
+filter <- rsums >= 5
+cat("Number of filtered out genes, not detected in at least 5 cells: ", sum(!filter), "\n")
+counts_mat <- counts_mat[filter,]
+gene_symbols_r <- gene_symbols_r[filter]
 
 type_factor_vector <- factor(labels_r)
 print(table(type_factor_vector))
@@ -63,7 +71,19 @@ for(type in levels(type_factor_vector)){
 		 # Create two counts matrices
 		 other_types_counts = counts_mat[, other_types_selection_vector]
 		 cur_type_counts = counts_mat[, cur_type_selection_vector]
+
+		 # Sample at most 20 from each group
+		 if (ncol(other_types_counts) > 20) {
+		    cat("\t", "Many OTHER, so sampling 20", "\n")
+		    other_types_counts <- other_types_counts[, sample(ncol(other_types_counts), 20)]
+		 }
+		 if (ncol(cur_type_counts) > 20) {
+		    cat("\t", "Many of current type, so sampling 20", "\n")
+		    cur_type_counts <- cur_type_counts[, sample(ncol(cur_type_counts), 20)]
+		 }
+		 
  		 combined_counts <- cbind(other_types_counts, cur_type_counts)
+                 print(str(combined_counts))
 
 		 grouping <- rep(c("OTHERS", type), c(ncol(other_types_counts), ncol(cur_type_counts)))
 		 groups <- factor(grouping)
@@ -71,45 +91,53 @@ for(type in levels(type_factor_vector)){
 		 cat("\t", "Fitting error models...", "\n")
 		 t0 <- proc.time()
 		 scde.fitted.model <- scde.error.models(counts=combined_counts, groups=groups, n.cores=n.cores, save.model.plots=F)
-		 proc.time() - t0
+		 print(proc.time() - t0)
 		 scde.prior <- scde.expression.prior(models=scde.fitted.model,counts=combined_counts)
 
-		 ediff <- scde.expression.difference(scde.fitted.model,counts,scde.prior,groups=groups,n.cores=n.cores)
+                 cat("\t", "Calculating differential expression...", "\n")
+                 t0 <- proc.time()
+		 ediff <- scde.expression.difference(scde.fitted.model,combined_counts,scde.prior,groups=groups,n.cores=n.cores)
+                 print(proc.time() - t0)
+                 
 		 p.values <- 2*pnorm(abs(ediff$Z),lower.tail=F) # 2-tailed p-value
 		 p.values.adj <- 2*pnorm(abs(ediff$cZ),lower.tail=F) # Adjusted to control for FDR
 		 significant.genes <- which(p.values.adj<0.05)
 		 cat("\t", "num significant genes: ", length(significant.genes), "\n")
+                 
+                 ord <- order(p.values.adj[significant.genes]) # order by p-value
+                 ## ord <- order(ediff[significant.genes, 2]) # order by log2 fold change
+		 de <- cbind(names(gene_symbols_r[significant.genes]),gene_symbols_r[significant.genes],ediff[significant.genes,1:3],p.values.adj[significant.genes])[ord,]
+		 colnames(de) <- c("EntrezID","symbol", "Lower bound","log2 fold change","Upper bound","p-value")
+                 print(str(de))
 
-		 ord <- order(p.values.adj[significant.genes]) # order by p-value
-		 de <- cbind(ediff[significant.genes,1:3],p.values.adj[significant.genes])[ord,]
-		 colnames(de) <- c("Lower bound","log2 fold change","Upper bound","p-value")
-
-		 print("Top 20 most significant DEGs:")
-		 de[1:20,]
+		 print("Top 20 significant DEGs by p-value:")
+		 print(de[1:20,])
 
 		 # Separate up and down regulated:
-		 upreg <- which(ediff[significant.genes, 2]>0.0)
-		 dwnreg <- which(ediff[significant.genes, 2]<0.0)
+		 upreg <- which(de[, 4]>0.0)
+		 dwnreg <- which(de[, 4]<0.0)
 		 
 		 cat("\t", "num significant upreg genes: ", length(upreg), "\n")
-		 ord <- order(p.values.adj[upreg])
-		 up_de <- cbind(ediff[upreg,1:3],p.values.adj[upreg])[ord,]
- 		 colnames(up_de) <- c("Lower bound","log2 fold change","Upper bound","p-value")
+		 ord <- order(de[upreg, 4])
+		 ##up_de <- cbind(names(gene_symbols_r[upreg]),gene_symbols_r[upreg],ediff[upreg,1:3],p.values.adj[upreg])[ord,]
+                 up_de <- de[upreg,][ord,]
+ 		 ##colnames(up_de) <- c("EntrezID","symbol","Lower bound","log2 fold change","Upper bound","p-value")
 		 
  		 cat("\t", "num significant downreg genes: ", length(dwnreg), "\n")
-		 ord <- order(p.values.adj[dwnreg])
-		 dwn_de <- cbind(ediff[dwnreg,1:3],p.values.adj[dwnreg])[ord,]
- 		 colnames(dwn_de) <- c("Lower bound","log2 fold change","Upper bound","p-value")
+		 ord <- order(de[dwnreg, 4])
+		 ##dwn_de <- cbind(names(gene_symbols_r[dwnreg]),gene_symbols_r[dwnreg],ediff[dwnreg,1:3],p.values.adj[dwnreg])[ord,]
+                 dwn_de <- de[dwnreg,][ord,]
+ 		 ##colnames(dwn_de) <- c("EntrezID","symbol","Lower bound","log2 fold change","Upper bound","p-value")
 		 
 		 name_for_results_file_all <- paste(name_for_results_file, "all", sep="_")
-  		 name_for_results_file_all <- paste(name_for_results_file_all, ".txt", sep="")
+  		 name_for_results_file_all <- paste(name_for_results_file_all, ".csv", sep="")
 		 name_for_results_file_up <- paste(name_for_results_file, "up", sep="_")
-   		 name_for_results_file_up <- paste(name_for_results_file_up, ".txt", sep="")
+   		 name_for_results_file_up <- paste(name_for_results_file_up, ".csv", sep="")
 		 name_for_results_file_dwn <- paste(name_for_results_file, "down", sep="_")
- 		 name_for_results_file_dwn <- paste(name_for_results_file_dwn, ".txt", sep="")
+ 		 name_for_results_file_dwn <- paste(name_for_results_file_dwn, ".csv", sep="")
 
-		 write.table(de, name_for_results_file_all, sep="\t")
- 		 write.table(up_de, name_for_results_file_all_up, sep="\t")
- 		 write.table(dwn_de, name_for_results_file_all_dwn, sep="\t")
+		 write.table(de, name_for_results_file_all, sep=",", row.names=TRUE, col.names=TRUE)
+ 		 write.table(up_de, name_for_results_file_up, sep=",", row.names=TRUE, col.names=TRUE)
+ 		 write.table(dwn_de, name_for_results_file_dwn, sep=",", row.names=TRUE, col.names=TRUE)
 	 }
 }
