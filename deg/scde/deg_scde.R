f5998022a9b6256bb96c483115c0d3a6b3f0a8ac
Amir Alavi <aalavi@andrew.cmu.edu>
2017-09-12 16:58:39 -0400

    Huge fix to DE analysis using SCDE
    Now do proper meta analysis by taking maximum pval from each experiment,
    also only does an experiment if there are at least 100 samples. Nicer
    printing. Runs as a command line script using Rscript.

--- deg/scde/deg_scde.R
+++ deg/scde/deg_scde.R
@@ -1,36 +1,49 @@
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 2) {
+    stop("Missing args! arg1=data_dir, arg2=ncores", call.=FALSE)
+} else {
+    data_dir <- args[1]
+    n.cores <- strtoi(args[2])
+}
+
+plot_pval_hist <- function(pvals, filename) {
+    png(filename)
+    hist(pvals, breaks=100, col="grey", main=paste(filename, "DE raw p-vals", sep=" "), xlab="Raw p-values")
+    dev.off()
+}
+
 library(scde)
 
-n.cores <- 16
 cat("Using ", n.cores, " cores", "\n")
-
-# Load R objects containing data into workspace
-load("counts_mat.gzip")		# counts_mat_r
-load("accessions.gzip") 	# accessions_r
-load("gene_symbols.gzip")	# gene_symbols_r
-load("labels.gzip")		# labels_r
+cat("Looking in ", data_dir, "\n") # Must have a data_dir variable in workspace!
+## Load R objects containing data into workspace
+load(paste(data_dir, "/", "counts_mat.gzip", sep=""))   # counts_mat_r
+load(paste(data_dir, "/", "accessions.gzip", sep=""))   # accessions_r
+load(paste(data_dir, "/", "gene_symbols.gzip", sep="")) # gene_symbols_r
+load(paste(data_dir, "/", "labels.gzip", sep=""))       # labels_r
 print("Done loading data objects")
 
 counts_mat <- data.matrix(counts_mat_r, rownames.force=TRUE)
 counts_mat <- t(counts_mat)
-#rownames(counts_mat) <- unname(gene_symbols_r)
+##rownames(counts_mat) <- unname(gene_symbols_r)
 print(str(counts_mat))
 print(dim(counts_mat))
 
 print("Filtering...")
-# Remove cells with fewer than 1.8e3 lib size
+## Remove cells with fewer than 1.8e3 lib size
 csums <- colSums(counts_mat > 0)
 filter <- csums >= 1.8e3
 cat("Number of filtered out cells, detected less than 1.8e3 genes: ", sum(!filter), "\n")
 counts_mat <- counts_mat[,filter]
 labels_r <- labels_r[filter]
 accessions_r <- accessions_r[filter]
-# Remove genes which have less than 10 reads accross all samples
+## Remove genes which have less than 10 reads accross all samples
 rsums <- rowSums(counts_mat)
 filter <- rsums >= 10
 cat("Number of filtered out genes, have less than 10 reads across all samples: ", sum(!filter), "\n")
 counts_mat <- counts_mat[filter,]
 gene_symbols_r <- gene_symbols_r[filter]
-# Remove genes that aren't detected in at least 5 cells
+## Remove genes that aren't detected in at least 5 cells
 rsums <- rowSums(counts_mat > 0)
 filter <- rsums >= 5
 cat("Number of filtered out genes, not detected in at least 5 cells: ", sum(!filter), "\n")
@@ -42,105 +55,137 @@ print(table(type_factor_vector))
 accessions_factor_vector <- factor(accessions_r)
 print(table(accessions_factor_vector))
 
-
-
-# Iterate over the types
-# For each one, we need to create some lists:
-#     others = all other cells that do not include the current type
-#     grp1, .., grpN = lists of cells of current type, grouped by experiment.
-#     	    	     Number of groups will depend on how many experiments had this cell type
+## Iterate over the types
+## For each one, we need to create some lists:
+##     others = all other cells that do not include the current type
+##     grp1, .., grpN = lists of cells of current type, grouped by experiment.
+##     	    	     Number of groups will depend on how many experiments had this cell type
 
 for(type in levels(type_factor_vector)){
-	 cat("Current type: ", type, "\n")
+    cat("Current type: ", type, "\n")
 
-	 cur_type_selection_vector <- type_factor_vector == type
-	 other_types_selection_vector <- !cur_type_selection_vector
-	 cat("\t", "Number of ", type, " cells: ", sum(cur_type_selection_vector), "\n")
-	 cat("\t", "Number of remaining cells: ", sum(other_types_selection_vector), "\n")
-	 
-	 cur_type_experiments = accessions_factor_vector[cur_type_selection_vector]
-	 uniq_cur_type_experiments = unique(cur_type_experiments)
-	 cat("\t", "Number of different experiments for ", type, " ", length(uniq_cur_type_experiments), "\n")
-	 exp_count <- 1
-	 for(exp in uniq_cur_type_experiments){
-	 	 name_for_results_file <- strsplit(type, split=" ")[[1]][1]
-	 	 name_for_results_file <- paste(name_for_results_file, exp_count, sep="_")
-                 exp_count <- exp_count + 1
+    cur_type_selection_vector <- type_factor_vector == type
+    other_types_selection_vector <- !cur_type_selection_vector
+    cat("\t", "Number of ", type, " cells: ", sum(cur_type_selection_vector), "\n")
+    cat("\t", "Number of remaining cells: ", sum(other_types_selection_vector), "\n")
+    
+    cur_type_experiments <- accessions_factor_vector[cur_type_selection_vector]
+    uniq_cur_type_experiments <- unique(cur_type_experiments)
+    cat("\t", "Number of different experiments for ", type, " ", length(uniq_cur_type_experiments), "\n")
 
-	 	 cur_exp_selection_vector <- accessions_factor_vector == exp
+    ## Collect the results of SCDE differential expression for each study, into a list for this cell type
+    deg_results <- list()
+    for(exp in uniq_cur_type_experiments){
+        cat("\t","\t", "Current experiment: ", exp, "\n")
+        filename <- strsplit(type, split=" ")[[1]][1]
+        filename <- paste(filename, exp, sep="_")
 
-		 # Create two counts matrices
-		 other_types_counts = counts_mat[, other_types_selection_vector]
-		 cur_type_counts = counts_mat[, cur_type_selection_vector]
+        ## Selection vector to extract samples for just this current experiment 
+        cur_exp_selection_vector <- accessions_factor_vector == exp
+        ## Selection vector to extract samples for just this current experiment AND this current cell type
+        cur_type_exp_sv <- cur_exp_selection_vector & cur_type_selection_vector
 
-                 ## Sample at most 100 from each group
-                 num_sample <- 100
-                 if (ncol(cur_type_counts) > num_sample) {
-		    cat("\t", "Many of current type, so sampling ", num_sample, "\n")
-		    cur_type_counts <- cur_type_counts[, sample(ncol(cur_type_counts), num_sample)]
-		 }
-		 if (ncol(other_types_counts) > ncol(cur_type_counts)) {
-		    cat("\t", "Many OTHER, so sampling ", ncol(cur_type_counts), "\n")
-		    other_types_counts <- other_types_counts[, sample(ncol(other_types_counts), ncol(cur_type_counts))]
-		 }
+        ## Ensure that each the sample size is large enough, if not, report that it wasn't and move on to next experiment
+        min_sample_size <- 100
+        num_samples <- sum(cur_type_exp_sv)
+        cat("\t", "\t", "Num samples: ", num_samples, "\n")
+        if (num_samples< min_sample_size) {
+            cat("\t", "\t", "Too few samples, skipping experiment", "\n")
+            break
+        }
 
-		 
- 		 combined_counts <- cbind(other_types_counts, cur_type_counts)
-                 print(str(combined_counts))
+        ## Create two counts matrices
+        other_types_counts = counts_mat[, other_types_selection_vector]
+        cur_type_counts = counts_mat[, cur_type_exp_sv]
 
-		 grouping <- rep(c("OTHERS", type), c(ncol(other_types_counts), ncol(cur_type_counts)))
-		 groups <- factor(grouping)
+        ## Sample at most 100 from each group
+        num_sample <- 100
+        if (ncol(cur_type_counts) > num_sample) {
+            cat("\t", "\t", "Many of current type, so sampling ", num_sample, "\n")
+            cur_type_counts <- cur_type_counts[, sample(ncol(cur_type_counts), num_sample)]
+        }
+        if (ncol(other_types_counts) > ncol(cur_type_counts)) {
+            cat("\t", "\t", "Many OTHER, so sampling ", ncol(cur_type_counts), "\n")
+            other_types_counts <- other_types_counts[, sample(ncol(other_types_counts), ncol(cur_type_counts))]
+        }
 
-		 cat("\t", "Fitting error models...", "\n")
-		 t0 <- proc.time()
-		 scde.fitted.model <- scde.error.models(counts=combined_counts, groups=groups, n.cores=n.cores, save.model.plots=F)
-		 print(proc.time() - t0)
-		 scde.prior <- scde.expression.prior(models=scde.fitted.model,counts=combined_counts)
+        combined_counts <- cbind(other_types_counts, cur_type_counts)
+        cat("\t", "\t", "str(combined_counts):", "\n")
+        print(str(combined_counts))
 
-                 cat("\t", "Calculating differential expression...", "\n")
-                 t0 <- proc.time()
-		 ediff <- scde.expression.difference(scde.fitted.model,combined_counts,scde.prior,groups=groups,n.cores=n.cores)
-                 print(proc.time() - t0)
-                 
-		 p.values <- 2*pnorm(abs(ediff$Z),lower.tail=F) # 2-tailed p-value
-		 p.values.adj <- 2*pnorm(abs(ediff$cZ),lower.tail=F) # Adjusted to control for FDR
-		 significant.genes <- which(p.values.adj<0.05)
-		 cat("\t", "num significant genes: ", length(significant.genes), "\n")
-                 
-                 ord <- order(p.values.adj[significant.genes]) # order by p-value
-                 ## ord <- order(ediff[significant.genes, 2]) # order by log2 fold change
-		 de <- cbind(names(gene_symbols_r[significant.genes]),gene_symbols_r[significant.genes],ediff[significant.genes,1:3],p.values.adj[significant.genes])[ord,]
-		 colnames(de) <- c("EntrezID","symbol", "Lower bound","log2 fold change","Upper bound","p-value")
-                 print(str(de))
+        grouping <- rep(c("OTHERS", type), c(ncol(other_types_counts), ncol(cur_type_counts)))
+        groups <- factor(grouping)
 
-		 print("Top 20 significant DEGs by p-value:")
-		 print(de[1:20,])
+        cat("\t", "\t", "Fitting error models...", "\n")
+        t0 <- proc.time()
+        scde.fitted.model <- scde.error.models(counts=combined_counts, groups=groups, n.cores=n.cores, save.model.plots=F)
+        print(proc.time() - t0)
+        scde.prior <- scde.expression.prior(models=scde.fitted.model,counts=combined_counts)
 
-		 # Separate up and down regulated:
-		 upreg <- which(de[, 4]>0.0)
-		 dwnreg <- which(de[, 4]<0.0)
-		 
-		 cat("\t", "num significant upreg genes: ", length(upreg), "\n")
-		 ord <- order(de[upreg, 4])
-		 ##up_de <- cbind(names(gene_symbols_r[upreg]),gene_symbols_r[upreg],ediff[upreg,1:3],p.values.adj[upreg])[ord,]
-                 up_de <- de[upreg,][ord,]
- 		 ##colnames(up_de) <- c("EntrezID","symbol","Lower bound","log2 fold change","Upper bound","p-value")
-		 
- 		 cat("\t", "num significant downreg genes: ", length(dwnreg), "\n")
-		 ord <- order(de[dwnreg, 4])
-		 ##dwn_de <- cbind(names(gene_symbols_r[dwnreg]),gene_symbols_r[dwnreg],ediff[dwnreg,1:3],p.values.adj[dwnreg])[ord,]
-                 dwn_de <- de[dwnreg,][ord,]
- 		 ##colnames(dwn_de) <- c("EntrezID","symbol","Lower bound","log2 fold change","Upper bound","p-value")
-		 
-		 name_for_results_file_all <- paste(name_for_results_file, "all", sep="_")
-  		 name_for_results_file_all <- paste(name_for_results_file_all, ".csv", sep="")
-		 name_for_results_file_up <- paste(name_for_results_file, "up", sep="_")
-   		 name_for_results_file_up <- paste(name_for_results_file_up, ".csv", sep="")
-		 name_for_results_file_dwn <- paste(name_for_results_file, "down", sep="_")
- 		 name_for_results_file_dwn <- paste(name_for_results_file_dwn, ".csv", sep="")
+        cat("\t", "\t", "Calculating differential expression...", "\n")
+        t0 <- proc.time()
+        ediff <- scde.expression.difference(scde.fitted.model,combined_counts,scde.prior,groups=groups,n.cores=n.cores)
+        print(proc.time() - t0)
+        
+        p.values <- 2*pnorm(abs(ediff$Z),lower.tail=F) # 2-tailed p-value
+        p.values.adj <- 2*pnorm(abs(ediff$cZ),lower.tail=F) # Adjusted to control for FDR
 
-		 write.table(de, name_for_results_file_all, sep=",", row.names=TRUE, col.names=TRUE)
- 		 write.table(up_de, name_for_results_file_up, sep=",", row.names=TRUE, col.names=TRUE)
- 		 write.table(dwn_de, name_for_results_file_dwn, sep=",", row.names=TRUE, col.names=TRUE)
-	 }
+        ## TODO: plot histogram of the raw p-values from this experiment.
+        plot_pval_hist(p.values, paste(filename, ".png", sep=""))
+        ##significant.genes <- which(p.values.adj<0.05)
+        ##cat("\t", "num significant genes: ", length(significant.genes), "\n")
+        
+        ##ord <- order(p.values.adj[significant.genes]) # order by p-value
+        ##de <- cbind(names(gene_symbols_r[significant.genes]),gene_symbols_r[significant.genes],ediff[significant.genes,1:3],p.values[significant.genes], p.values.adj[significant.genes])[ord,]
+        ##colnames(de) <- c("EntrezID","Symbol", "Lower_bound","Log2_fold_change","Upper_bound","Raw_p_value", "Adj_p_value")
+
+        de <- cbind(names(gene_symbols_r),gene_symbols_r,ediff[,1:3],p.values, p.values.adj)
+        colnames(de) <- c("EntrezID", "Symbol", "Lower_bound", "Log2_fold_change", "Upper_bound", "Raw_p_value", "Adj_p_value")
+        
+        deg_results <- c(deg_results, de) # Add the results of this study to the collection for this cell type
+        ## Also save the results of this experiment in its own file, to have on record
+
+        filename <- paste(filename, ".csv", sep="")
+        write.table(de, filename, sep=",", row.names=TRUE, col.names=TRUE)
+    }
+    
+    ## Now, for this cell type, if we had more than one study, do a meta-analysis by taking maximum adjusted p-value as the p-value
+    ## and the average of the log2(foldchange)
+    if (length(deg_results) == 0) {
+        cat("\t", "No experiments done for this type", "\n")
+    } else {
+        max_adj_pvals = integer(dim(counts_mat)[1])
+        avg_fold_change = integer(dim(counts_mat)[1])
+        for (results in deg_results) {
+            max_adj_pvals <- pmax(max_adj_pvals, results$Adj_p_value)
+            avg_fold_change <- avg_fold_change + results$Log2_fold_change
+        }
+        avg_fold_change <- avg_fold_change / length(deg_results)
+
+        ## Now, do significance and FDR adjustment using these new p-values
+        ## TODO: should we re-adjust p-vals?
+        ##meta_adj_pvals <- p.adjust(max_adj_pvals, method="BH")
+        meta_adj_pvals <- max_adj_pvals
+        sig_genes_sv<- meta_adj_pvals < 0.05
+        cat("\t", "Num sig DEGs after meta analysis: ", sum(sig_genes_sv), "\n")
+
+        ## Check consistency of Log2FoldChange sign accross experiments
+        fold_change_signs = integer(dim(counts_mat)[1])
+        for (results in deg_results) {
+            fold_changes_sign <- fold_changes_sign + sign(results$Log2_fold_change)
+        }
+        fold_change_consistent_sv <- abs(fold_change_signs) == length(deg_results)
+        sig_and_consistent_sv <- sig_genes_sv & fold_change_consistent_sv
+        cat("\t", "Num of sig DEGs with consistent foldchange accross experiments: ", sum(sig_and_consistent_sv), "\n")
+        
+        ord <- order(meta_adj_pvals[sig_and_consistent_sv]) # order by p-value
+        final_results <- cbind(names(gene_symbols_r[sig_and_consistent_sv]), gene_symbols_r[sig_and_consistent_sv], avg_fold_change[sig_and_consistent_sv], meta_adj_pvals[sig_and_consistent_sv])[ord,]
+        colnames(final_results) <- c("EntrezID", "Symbol", "Avg_log2_fold_change", "Max_adj_p_value")
+        ## Finally, Save the results
+        filename <- strsplit(type, split=" ")[[1]][1]
+        filename <- paste(filename, "meta", sep="_")
+        filename <- paste(filename, ".csv", sep="")
+        write.table(final_results, filename, sep=",", row.names=TRUE, col.names=TRUE)
+    }
+    
 }
