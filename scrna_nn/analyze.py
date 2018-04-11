ccb85b176c4c4a556d731739dbc48d8500324c08
Amir Alavi <aalavi@andrew.cmu.edu>
2018-04-10 20:42:06 -0400

    Added anlayze script

---
+++ scrna_nn/analyze.py
@@ -0,0 +1,55 @@
+from os.path import join
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+from .data_container import DataContainer
+from .reduce import _reduce_helper
+from . import util
+
+
+def visualize(data, working_dir):
+    original_dims = data.shape[1]
+    pca = PCA(n_components=2)
+    pca_reduced = pca.fit_transform(data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(pca_reduced[:,0], pca_reduced[:,1])
+    plt.title("{:d} reduced by PCA".format(original_dims))
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    fig.savefig(join(working_dir, "pca_{:d}.png".format(original_dims)))
+    plt.close()
+
+    tsne = TSNE(n_components=2)
+    tsne_reduced = tsne.fit_transform(data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(tsne_reduced[:,0], tsne_reduced[:,1])
+    plt.title("{:d} reduced by t-SNE".format(original_dims))
+    plt.xlabel('component 1')
+    plt.ylabel('component 2')
+    fig.savefig(join(working_dir, "tsne_{:d}.png".format(original_dims)))
+    plt.close()
+
+
+# analyze <model> <raw_data> <raw_database> --out=...
+def analyze(args):
+    working_dir_path = util.create_working_directory(args['--out'], "analyses/")
+    query_data = DataContainer(args['<query>'])
+    #database_data = DataContainer(args['<database>'])
+    query = query_data.get_expression_mat()
+    #database = database_data.get_expression_mat()
+
+    # First, visualize the unreduced query data
+    visualize(query, working_dir_path)
+
+    # Then embed it and visualize it
+    query_reduced, _ = _reduce_helper(args['<trained_model_folder>'], args['<query>'])
+    visualize(query_reduced, working_dir_path)
+
+    
+
+    
--- scrna_nn/reduce.py
+++ scrna_nn/reduce.py
@@ -29,33 +29,34 @@ def save_reduced_data_to_h5(filename, X_reduced, data_container, save_metadata):
     h5_store.close()
 
 
-def reduce(args):
-    training_args_path = join(args['<trained_model_folder>'], "command_line_args.json")
+def _reduce_helper(trained_model_folder, data_to_reduce):
+    training_args_path = join(trained_model_folder, "command_line_args.json")
     with open(training_args_path, 'r') as fp:
         training_args = json.load(fp)
     # Must ensure that we use the same normalizations/standardization from when model was trained
     mean = None
     std = None
-    if training_args['--gn']:
-        mean = pd.read_pickle(join(args['<trained_model_folder>'], "mean.p"))
-        std = pd.read_pickle(join(args['<trained_model_folder>'], "std.p"))
-    data_container = DataContainer(args['--data'], sample_normalize=training_args['--sn'], feature_normalize=training_args['--gn'], feature_mean=mean, feature_std=std)
+    feature_normalize = False
+    if '--gn' in training_args and training_args['--gn']:
+        feature_normalize = True
+        mean = pd.read_pickle(join(trained_model_folder, "mean.p"))
+        std = pd.read_pickle(join(trained_model_folder, "std.p"))
+    data_container = DataContainer(data_to_reduce, sample_normalize=training_args['--sn'], feature_normalize=feature_normalize, feature_mean=mean, feature_std=std)
     X = data_container.get_expression_mat()
-    model_base_path = args['<trained_model_folder>']
     if training_args['--nn']:
         if '--triplet' in training_args and training_args['--triplet']:
             triplet_batch_size = int(training_args['--batch_hard_P'])*int(training_args['--batch_hard_K'])
-            model = nn.load_trained_nn(join(model_base_path, "model.h5"), triplet_loss_batch_size=triplet_batch_size)
+            model = nn.load_trained_nn(join(trained_model_folder, "model.h5"), triplet_loss_batch_size=triplet_batch_size)
         elif training_args['--siamese']:
             dynamic_margin=-1
             if '--dynMargin' in training_args:
                 dynamic_margin = float(training_args['--dynMargin'])
-            model = nn.load_trained_nn(join(model_base_path, "model.h5"), dynamic_margin=dynamic_margin, siamese=True)
+            model = nn.load_trained_nn(join(trained_model_folder, "model.h5"), dynamic_margin=dynamic_margin, siamese=True)
             if training_args['--checkpoints']:
                 # HACK, TODO: account for this beforehand
                 model = model.layers[2]
         else:
-            model = nn.load_trained_nn(join(model_base_path, "model.h5"))
+            model = nn.load_trained_nn(join(trained_model_folder, "model.h5"))
         print(model.summary())
         # use the last hidden layer of the model as a lower-dimensional representation:
         if training_args['--siamese']:
@@ -70,11 +71,14 @@ def reduce(args):
         X_transformed = get_activations([X])[0]
     else:
         # Use PCA
-        with open(join(model_base_path, "pca.p"), 'rb') as f:
+        with open(join(trained_model_folder, "pca.p"), 'rb') as f:
             model = pickle.load(f)
         X_transformed = model.transform(X)
     print("reduced dimensions to: ", X_transformed.shape)
-    model_type = training_args['--nn'] if training_args['--nn'] is not None else "pca"
-    save_reduced_data_to_h5(args['--out'], X_transformed, data_container, args['--save_meta'])
+    return X_transformed, data_container
+    
+def reduce(args):
+    X_transformed, original_data_container = _reduce_helper(args['<trained_model_folder>'], args['--data'])
+    save_reduced_data_to_h5(args['--out'], X_transformed, original_data_container, args['--save_meta'])
     # with open(join(working_dir_path, "training_command_line_args.json"), 'w') as fp:
     #     json.dump(training_args, fp)
